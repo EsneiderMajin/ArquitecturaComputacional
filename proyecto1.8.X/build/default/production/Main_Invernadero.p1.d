aaa4639289a28e6730ae8ede18888027adfff427
EsneiderMajin <99764615+EsneiderMajin@users.noreply.github.com>
2022-03-09 22:38:25 -0500

    Add files via upload

---
+++ proyecto1.8.X/build/default/production/Main_Invernadero.p1.d
@@ -0,0 +1,8 @@
+build/default/production/Main_Invernadero.p1:  \
+Main_Invernadero.c  \
+Configuracion.h  \
+conectores.h  \
+LCD.h  \
+Config.h  \
+keypad.h  \
+DHT11.h 
---
+++ proyecto1.8.X/build/default/production/keypad.p1.d
@@ -0,0 +1,6 @@
+build/default/production/keypad.p1:  \
+keypad.c  \
+keypad.h  \
+Config.h  \
+conectores.h  \
+Configuracion.h 
